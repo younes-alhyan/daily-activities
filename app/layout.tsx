d032d7ed1cb74ac69a4d28cafce6e019cb542fe7
younes-alhyan <younesalhyan555@gmail.com>
2026-02-09 16:00:00 +0100

    feat(layout): setup root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from "./contexts/AuthContext";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
