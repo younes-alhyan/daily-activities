26274400f4bc630573d443ff6297149d4cfc7abd
younes-alhyan <gameryounes555@gmail.com>
2026-02-14 04:38:59 +0100

    refactor(types): refactor user types

--- types/user.types.ts
+++ types/user.types.ts
@@ -1,12 +1,16 @@
 import { Types } from "mongoose";
 
-interface UserInterface {
+export interface UserInterface {
   username: string;
   password: string;
 }
-export interface User extends UserInterface {
-  id: string;
-}
 export interface UserDoc extends UserInterface {
   _id: Types.ObjectId;
+}
+export interface User {
+  id: string;
+  username: string;
+}
+export interface UserLogin extends User {
+  token: string;
 }
