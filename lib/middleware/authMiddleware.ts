6d9eae909a3a9ce277524c00bd59e70ab6a2ab5f
younes-alhyan <younesalhyan555@gmail.com>
2026-02-15 13:00:00 +0000

    feat(middlewares): add error handling to auth middleware

--- lib/middleware/authMiddleware.ts
+++ lib/middleware/authMiddleware.ts
@@ -1,13 +1,21 @@
-import jwt from "jsonwebtoken";
+import { connectDB } from "@/lib/db/connect";
+import { verifyToken } from "@/lib/utils/jwt";
+import { httpErrors } from "@/lib/http/httpErrors";
+import { UserModel } from "@/server/models/user.model";
 
-export function authMiddleware(authHeader: string) {
-  if (!authHeader?.startsWith("Bearer ")) return false;
+export async function authMiddleware(authHeader: string) {
+  await connectDB();
+
+  if (!authHeader?.startsWith("Bearer "))
+    throw httpErrors.UNAUTHORIZED_ERROR("Invalid Authorization header");
+
   const token = authHeader.replace("Bearer ", "");
 
-  try {
-    jwt.verify(token, process.env.TOKEN_SECRET!);
-    return true;
-  } catch (err) {
-    return false;
-  }
+  const payload = verifyToken(token);
+
+  const userExists = await UserModel.exists({ _id: payload.userId });
+
+  if (!userExists) throw httpErrors.UNAUTHORIZED_ERROR("User not found");
+
+  return payload.userId;
 }
