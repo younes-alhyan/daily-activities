42b082c54b71f66dd127c5c0ef97cb4a8b542e0f
younes-alhyan <younesalhyan555@gmail.com>
2026-02-16 15:00:00 +0000

    refactor(middlewares): accept full Request object instead of authorization header

--- lib/middleware/authMiddleware.ts
+++ lib/middleware/authMiddleware.ts
@@ -3,18 +3,17 @@ import { verifyToken } from "@/lib/utils/jwt";
 import { httpErrors } from "@/lib/http/httpErrors";
 import { UserModel } from "@/server/models/user.model";
 
-export async function authMiddleware(authHeader: string) {
+export async function authMiddleware(req: Request) {
   await connectDB();
 
+  const authHeader = req.headers.get("Authorization");
   if (!authHeader?.startsWith("Bearer "))
     throw httpErrors.UNAUTHORIZED_ERROR("Invalid Authorization header");
 
   const token = authHeader.replace("Bearer ", "");
-
   const payload = verifyToken(token);
 
   const userExists = await UserModel.exists({ _id: payload.userId });
-
   if (!userExists) throw httpErrors.UNAUTHORIZED_ERROR("User not found");
 
   return payload.userId;
