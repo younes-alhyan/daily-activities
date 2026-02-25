98662201668af22603b04a71a7fcb6ca6e19a9f5
younes-alhyan <younesalhyan555@gmail.com>
2026-02-25 05:42:49 +0000

    refactor(middlewares): remove user existing check

--- lib/middlewares/authMiddleware.ts
+++ lib/middlewares/authMiddleware.ts
@@ -1,20 +1,9 @@
 import { Jwt } from "@/lib/utils/jwt";
-import { connectDB } from "@/lib/db/connect";
-import { httpErrors } from "@/lib/http/httpErrors";
-import { toObjectId } from "@/lib/utils/toObjectId";
-import { UserModel } from "@/server/models/user.model";
 
-export async function authMiddleware(req: Request, isRefresh?: true) {
-  await connectDB();
-
+export const authMiddleware = (req: Request, isRefresh?: true) => {
   const { get, verify } = isRefresh ? Jwt.refreshToken : Jwt.accessToken;
   const token = get(req);
   const payload = verify(token);
 
-  const userExists = await UserModel.exists({
-    _id: toObjectId(payload.userId),
-  });
-  if (!userExists) throw httpErrors.UNAUTHORIZED_ERROR("User not found");
-
   return payload.userId;
-}
+};
