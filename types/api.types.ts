7cb21e7283f9a999bf7e1f0929e32fbd88a6d8f2
younes-alhyan <younesalhyan555@gmail.com>
2026-03-14 12:00:00 +0000

    feat(types): move ApiError type and add ApiHook types

--- types/api.types.ts
+++ types/api.types.ts
@@ -1,17 +1,47 @@
 type HTTP_METHOD = "GET" | "POST" | "PATCH" | "DELETE";
 
-export type ApiRequest<TokenT extends boolean = false, BodyT = undefined> = {
+export type ApiRequest<TokenT extends boolean = false, BodyT = null> = {
   method: HTTP_METHOD;
   url: string;
 } & (TokenT extends true ? { accessToken: string } : {}) &
-  (BodyT extends undefined ? {} : { body: BodyT });
+  (BodyT extends null ? {} : { body: BodyT });
 
-export type ApiResponse<SuccessT extends boolean = false, DataT = undefined> = {
+export type ApiResponse<SuccessT extends boolean = false, DataT = null> = {
   ok: SuccessT;
   status: number;
   message: string;
 } & (SuccessT extends false
   ? { code: string }
-  : DataT extends undefined
+  : DataT extends null
     ? {}
     : { data: DataT });
+
+export class ApiError extends Error {
+  response: ApiResponse<false>;
+  constructor(response: ApiResponse<false>) {
+    super(response.message);
+    this.name = "ApiError";
+    this.response = response;
+  }
+}
+
+export type ApiHookDef = {
+  TokenT: boolean;
+  HookArgsT: Record<string, any> | null;
+  InputArgsT: Record<string, any> | null;
+  DataT: any;
+};
+
+export type ApiHook<T extends ApiHookDef> = {
+  Def: T;
+  Hook: {
+    isLoading: boolean;
+    error: ApiError | null;
+    clearError: () => void;
+    call: T["InputArgsT"] extends null
+      ? () => Promise<ApiResponse<true, T["DataT"]> | undefined>
+      : (
+          input: T["InputArgsT"],
+        ) => Promise<ApiResponse<true, T["DataT"]> | undefined>;
+  };
+};
