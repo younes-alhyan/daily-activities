e33ab2fb228ef9be70258a3b914fd70df15c1a55
younes-alhyan <younesalhyan555@gmail.com>
2026-03-16 08:35:00 +0000

    refactor(pages): refactor auth page to use shadcn components

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,24 @@
-import { AuthView } from "@/client/views/AuthView";
+"use client";
+import { useState } from "react";
+import { useAuth } from "@/client/contexts/AuthContext";
+import { AuthCard } from "./components/AuthCard";
 
 export default function AuthPage() {
-  return <AuthView />;
+  const [mode, setMode] = useState<"signup" | "login">("signup");
+  const { signup, login } = useAuth();
+
+  const authAction = mode === "signup" ? signup : login;
+
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center px-4">
+      <AuthCard
+        mode={mode}
+        setMode={setMode}
+        authHandler={(body) => {authAction.call({ body })}}
+        isLoading={authAction.isLoading}
+        error={authAction.error && authAction.error.response.message}
+        clearError={authAction.clearError}
+      />
+    </div>
+  );
 }
