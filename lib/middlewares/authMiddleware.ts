c4d5ccc29a26352f1b948809617ec4a8b5cdf91e
younes-alhyan <younesalhyan555@gmail.com>
2026-03-20 00:11:38 +0000

    refactor(middleware): update authMiddleware to use NextRequest instead of Request

--- lib/middlewares/authMiddleware.ts
+++ lib/middlewares/authMiddleware.ts
@@ -1,6 +1,7 @@
 import { Jwt } from "@/lib/utils/jwt";
+import type { NextRequest } from "next/server";
 
-export const authMiddleware = (req: Request, isRefresh?: true) => {
+export const authMiddleware = (req: NextRequest, isRefresh?: true) => {
   const { get, verify } = isRefresh ? Jwt.refreshToken : Jwt.accessToken;
   const token = get(req);
   const payload = verify(token);
